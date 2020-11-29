57aec2f9cb6f93e271f0e57e7fb25a8e7e415a46
Joshua Driesman <jdriesman@outlook.com>
2020-11-29 14:58:18 -0500

    basic CRUD for to do items

--- src/main.go
+++ src/main.go
@@ -1,6 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var todoList = make(map[string]bool)
+
+func checkAndHandleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return
+}
+
+func addTodo(newTodo string) {
+	_, present := todoList[newTodo]
+	if present {
+		fmt.Println("To do item already exists in list! Please add another.")
+		return
+	}
+	todoList[newTodo] = false
+}
+
+func listTodo() {
+	for todo, status := range todoList {
+		fmt.Printf("%s: ", todo)
+		if status {
+			fmt.Print("Completed\n")
+		} else {
+			fmt.Print("Incomplete\n")
+		}
+	}
+}
+
+func enumerateTodo() []string {
+	enumeratedTodoItems := make([]string, len(todoList))
+	index := 0
+	for todoItem := range todoList {
+		enumeratedTodoItems[index] = todoItem
+		index++
+	}
+	return enumeratedTodoItems
+}
+
+func selectTodo() (string, error) {
+	enumeratedTodoItems := enumerateTodo()
+	for i, todoItem := range enumeratedTodoItems {
+		fmt.Printf("%d: %s\n", i, todoItem)
+	}
+	fmt.Print("Enter number: ")
+
+	var indexOfTodoToToggle string
+	_, err := fmt.Scanln(&indexOfTodoToToggle)
+	checkAndHandleError(err)
+
+	intIndexOfTodoToToggle, err := strconv.Atoi(indexOfTodoToToggle)
+
+	if err != nil || intIndexOfTodoToToggle > len(enumeratedTodoItems) {
+		fmt.Println("You did not enter the number for a valid to do item!")
+		return "", errors.New("Bad")
+	}
+
+	return enumeratedTodoItems[intIndexOfTodoToToggle], nil
+}
+
+func toggleTodo() {
+	fmt.Println("Enter which to do item you'd like to toggle?")
+	itemToToggle, err := selectTodo()
+	if err != nil {
+		return
+	}
+	todoList[itemToToggle] = !todoList[itemToToggle]
+}
+
+func deleteTodo() {
+	fmt.Println("Enter which to do item you'd like to delete?")
+	itemToDelete, err := selectTodo()
+	if err != nil {
+		return
+	}
+	delete(todoList, itemToDelete)
+}
 
 func promptAndRead() {
 	for {
@@ -10,14 +94,29 @@ func promptAndRead() {
 
 		_, err := fmt.Scanln(&command)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+		checkAndHandleError(err)
 
 		switch command {
+		case "a":
+			fmt.Print("Please enter the task you'd like to add to your to do list: ")
+			var newTodo string
+
+			_, err := fmt.Scanln(&newTodo)
+			checkAndHandleError(err)
+
+			addTodo(newTodo)
+		case "l":
+			listTodo()
+		case "t":
+			toggleTodo()
+		case "d":
+			deleteTodo()
 		case "h":
 			fmt.Println("Here are all the commands this program supports!")
+			fmt.Println("a: add an item to your to do list")
+			fmt.Println("l: list all to do items")
+			fmt.Println("t: toggle the status of a to do item")
+			fmt.Println("d: delete an item from the to do list")
 			fmt.Println("h: print this help prompt")
 			fmt.Println("q: quit this program")
 		case "q":
