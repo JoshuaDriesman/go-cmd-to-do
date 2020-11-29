8e81824081eb909d36b243f2203ad499bd55ddf5
Joshua Driesman <jdriesman@outlook.com>
2020-11-29 14:25:53 -0500

    Basic eval print loop working

--- src/main.go
+++ src/main.go
@@ -2,7 +2,33 @@ package main
 
 import "fmt"
 
+func promptAndRead() {
+	for {
+		fmt.Print("Enter a command (type 'h' for a list of commands): ")
+
+		var command string
+
+		_, err := fmt.Scanln(&command)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		switch command {
+		case "h":
+			fmt.Println("Here are all the commands this program supports!")
+			fmt.Println("h: print this help prompt")
+			fmt.Println("q: quit this program")
+		case "q":
+			return
+		default:
+			fmt.Printf("%s is not a valid command, type 'h' for a list of valid commands\n", command)
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Welcome to GO To Do!")
-	fmt.Println("Enter a command (type h for a list of commands): ")
+	promptAndRead()
 }
